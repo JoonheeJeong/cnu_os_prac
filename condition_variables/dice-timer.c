34b346480427241f49623665ec3162b093cd0bf3
Joonhee Jeong <jeonggoo75@gmail.com>
2022-04-13 09:43:58 +0900

    feat: debug dice-timer.c

--- condition_variables/dice-timer.c
+++ condition_variables/dice-timer.c
@@ -4,11 +4,11 @@
 #include <time.h> // time
 #include <unistd.h> // usleep
 
-#define NLOOPS 5
+#define NLOOPS 1
 
 int num = 0;
 int generated = 0;
-pthread_mutex_t mutex_num = PTHREAD_MUTEX_INITIALIZER;
+//pthread_mutex_t mutex_num = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex_gen = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond_gen = PTHREAD_COND_INITIALIZER;
 
@@ -17,18 +17,18 @@ void *child(void *arg)
     printf("child: begin\n");
     srand(time(NULL));
     for (int i = 0; i < NLOOPS; ++i) {
-	pthread_mutex_lock(&mutex_num);
 	pthread_mutex_lock(&mutex_gen);
+//	pthread_mutex_lock(&mutex_num);
 	num = (rand() % 6) + 1;
 	generated = 1;
 	pthread_cond_signal(&cond_gen);
+//	pthread_mutex_unlock(&mutex_num);
 	pthread_mutex_unlock(&mutex_gen);
-	pthread_mutex_unlock(&mutex_num);
 	sleep(1);
     }
 
     printf("child: return\n");
-    return 0;
+    return NULL;
 }
 
 int main()
@@ -41,13 +41,13 @@ int main()
 	pthread_mutex_lock(&mutex_gen);
 	while (!generated)
 	    pthread_cond_wait(&cond_gen, &mutex_gen);
-	pthread_mutex_lock(&mutex_num);
+//	pthread_mutex_lock(&mutex_num);
 	printf("parent: %d\n", num);
 	generated = 0;
-	pthread_mutex_unlock(&mutex_num);
+//	pthread_mutex_unlock(&mutex_num);
 	pthread_mutex_unlock(&mutex_gen);
     }
-
+//    pthread_join(t1, NULL);
     printf("parent: return\n");
     return 0;
 }
